204b641acf01840bfb93cf50951231b72bd434a6
Alejandro <alejandrojrosas2001@gmail.com>
2024-05-13 21:12:01 -0400

    chore: more environmental configurations

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  title = 'client'
 }
