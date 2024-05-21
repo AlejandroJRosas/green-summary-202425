e030b5a3ff35b04dd03c1f201ff1559c54ccf5cd
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-05-20 21:57:08 -0400

    feat: add api tag for better swagger documentation

--- apps/server/src/core/users/users.controller.ts
+++ apps/server/src/core/users/users.controller.ts
@@ -13,7 +13,9 @@ import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { FindOneParams } from './dto/find-one-params.dto'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
