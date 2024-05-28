56d3fbe4bb8b534437af6307b775ee5c724262c4
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-05-28 18:34:00 -0400

    docs: index to filtering segment API doc from swagger

--- apps/server/src/shared/filtering/filters-segment.dto.ts
+++ apps/server/src/shared/filtering/filters-segment.dto.ts
@@ -5,7 +5,16 @@ import { ApiProperty } from '@nestjs/swagger'
 import { IsOptional } from 'class-validator'
 
 export class FiltersSegmentDto {
-  @ApiProperty({ type: String, required: false })
+  @ApiProperty({
+    type: String,
+    required: false,
+    externalDocs: {
+      description: 'Filtering Segment API Documentation',
+      url: 'https://github.com/AlejandroJRosas/green-summary-202425/tree/main/apps/server/src/shared/filtering'
+    },
+    description:
+      'More info (https://github.com/AlejandroJRosas/green-summary-202425/tree/main/apps/server/src/shared/filtering).'
+  })
   @Transform(
     (segment) => {
       return parseFiltersSegment(segment.value)
