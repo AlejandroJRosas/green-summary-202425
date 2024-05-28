1ba8e94cd7a996951c33a2541d985b2c96fe5da1
Chocopaula <mariapforero2003@gmail.com>
2024-05-28 00:24:40 -0400

    feat: add create department

--- apps/client/src/app/core/department/department.component.ts
+++ apps/client/src/app/core/department/department.component.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from '@angular/common'
-import { Component, OnInit } from '@angular/core'
+import { Component, Inject, OnInit } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 import { TableModule } from 'primeng/table'
 import { ButtonModule } from 'primeng/button'
 import { SkeletonModule } from 'primeng/skeleton'
+import { ConfirmDialogModule } from 'primeng/confirmdialog'
+import { ConfirmationService } from 'primeng/api'
+import { DialogModule } from 'primeng/dialog'
+import {
+  CreateDepartmentPayload,
+  DepartmentService
+} from '../../services/department.service'
+import { InputTextModule } from 'primeng/inputtext'
+import { Toast } from '../../common/toast/toast.component'
 import { Department } from './department.type'
-import { DepartmentService } from '../../services/department.service'
 
 @Component({
   selector: 'app-department',
@@ -15,49 +23,122 @@ import { DepartmentService } from '../../services/department.service'
     TableModule,
     CommonModule,
     FormsModule,
-    SkeletonModule
+    SkeletonModule,
+    ConfirmDialogModule,
+    Toast,
+    DialogModule,
+    InputTextModule
   ],
-  templateUrl: './department.component.html'
+  templateUrl: './department.component.html',
+  providers: [ConfirmationService]
 })
 export class DepartmentComponent implements OnInit {
-  constructor(private departmentService: DepartmentService) {}
+  constructor(
+    @Inject(Toast) private toast: Toast,
+    private confirmationService: ConfirmationService,
+    private departmentService: DepartmentService
+  ) {}
 
   isDeletingDepartments = false
   isFetchingDepartments = false
+  visible: boolean = false
 
-  departments: Department[] | string[] = []
+  message: string = ''
+  departments: Department[] = []
 
-  selectedDepartment!: Department
+  department: CreateDepartmentPayload = {
+    fullName: '',
+    email: '',
+    password: '123',
+    type: 'department'
+  }
 
+  skeletons: object[] = new Array(5).fill({})
+
+  ngOnInit() {
+    this.isFetchingDepartments = true
+    this.departmentService.getAll().subscribe({
+      next: (res) => {
+        // console.log(res);
+        this.isFetchingDepartments = false
+        this.departments = res.data.items
+      },
+      error: (e) => {
+        console.error(e)
+        this.isFetchingDepartments = false
+      }
+    })
+  }
+
+  showDialogCreate() {
+    this.visible = true
+  }
+  showDialogEdit() {
+    this.visible = true
+  }
+
+  confirmationDelete(event: Event, id: number, name: string) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: `¿Estás seguro de que quieres eliminar el departamento <strong>${name}</strong>?`,
+      header: 'Eliminar departamento',
+      icon: 'pi pi-info-circle',
+      acceptButtonStyleClass: 'p-button-danger p-button-text',
+      rejectButtonStyleClass: 'p-button-text p-button-text',
+      acceptIcon: 'none',
+      rejectIcon: 'none',
+
+      accept: () => {
+        this.toast.show(
+          'success',
+          'Eliminado',
+          'Departamento eliminado con éxito'
+        )
+        this.onDelete(id)
+      },
+      reject: () => {
+        this.toast.show('error', 'Rechazado', 'Haz rechazado la eliminación')
+      }
+    })
+  }
+
+  onCreate() {
+    this.departmentService.create(this.department).subscribe({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      next: (res) => {
+        this.toast.show('success', 'Creado', 'Departamento creado con éxito')
+      },
+      error: (e) => {
+        console.error(e)
+        this.toast.show('error', 'Error', 'Error creando departamento')
+      }
+    })
+  }
+
+  onEdit(id: number) {
+    this.departmentService.edit(id, this.department).subscribe({
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      next: (res) => {
+        this.toast.show('success', 'Editado', 'Departamento editado con éxito')
+      },
+      error: (e) => {
+        console.error(e)
+        this.toast.show('error', 'Error', 'Error editando el departamento')
+      }
+    })
+  }
   onDelete(id: number) {
     this.isDeletingDepartments = true
     this.departmentService.delete(id).subscribe({
       next: (res) => {
         console.log(res)
-        // this.departments = this.departments.filter(
-        //   (department) => department.id !== id
-        // )
-        this.isDeletingDepartments = false
+        this.departments = this.departments.filter(
+          (department) => department.id !== id
+        )
       },
       error: (e) => {
         console.error(e)
         this.isDeletingDepartments = false
-      }
-    })
-  }
-
-  ngOnInit() {
-    this.departments = Array(8).map(() => 'xd')
-    this.isFetchingDepartments = true
-    this.departmentService.getAll().subscribe({
-      next: (res) => {
-        // console.log(res);
-        this.departments = res.data.items
-        this.isFetchingDepartments = false
-      },
-      error: (e) => {
-        console.error(e)
-        this.isFetchingDepartments = false
       }
     })
   }
--- apps/client/src/app/services/department.service.ts
+++ apps/client/src/app/services/department.service.ts
@@ -11,9 +11,25 @@ export class DepartmentService {
   getAll(): Observable<ResponseType> {
     return this.http.get<ResponseType>(`${BaseUrl}/users`)
   }
+  create(departmentPayload: CreateDepartmentPayload): Observable<any> {
+    return this.http.post<any>(`${BaseUrl}/users`, departmentPayload)
+  }
+  edit(
+    id: number,
+    departmentPayload: CreateDepartmentPayload
+  ): Observable<any> {
+    return this.http.patch<any>(`${BaseUrl}/users/${id}`, departmentPayload)
+  }
   delete(id: number): Observable<any> {
     return this.http.delete<any>(`${BaseUrl}/users/${id}`)
   }
+}
+
+export type CreateDepartmentPayload = {
+  fullName: string
+  email: string
+  password: string
+  type: 'department'
 }
 
 type ResponseType = {
