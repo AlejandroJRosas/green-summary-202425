72962a10397f181a54356f1367fa8b4bf19aa16e
Alejandro <alejandrojrosas2001@gmail.com>
2024-06-01 21:27:07 -0400

    fix(login-guard): issue that redirects indefinetly and doesnt let you login

--- apps/client/src/app/guards/login.guard.ts
+++ apps/client/src/app/guards/login.guard.ts
@@ -1,7 +1,6 @@
 import { inject } from '@angular/core'
 import { CanActivateFn, Router } from '@angular/router'
 import { AuthService } from '../services/auth.service'
-import { Toast } from '../common/toast/toast.component'
 
 export const loginGuard: CanActivateFn = () => {
   const authService = inject(AuthService)
@@ -10,9 +9,6 @@ export const loginGuard: CanActivateFn = () => {
     const url = router.createUrlTree(['/home'])
     return url
   } else {
-    const url = router.createUrlTree(['/login'])
-    const toast = inject(Toast)
-    toast.show('error', 'Error', '¡Ups, parece que no has iniciado sesión aún!')
-    return url
+    return true
   }
 }
