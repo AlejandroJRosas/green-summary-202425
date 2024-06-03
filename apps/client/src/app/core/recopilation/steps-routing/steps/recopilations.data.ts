58f0cfa1c4a136644d2ae92bdf24ed8d6bbe84cb
Chocopaula <mariapforero2003@gmail.com>
2024-06-03 19:27:06 -0400

    feat: add simulated data “categories”, “recopilations” and “data” which contains information on indicators, categories and criteria.

---
+++ apps/client/src/app/core/recopilation/steps-routing/steps/recopilations.data.ts
@@ -0,0 +1,14 @@
+export const recopilations = [
+  {
+    id: 1,
+    name: 'Recopilación 1'
+  },
+  {
+    id: 2,
+    name: 'Recopilación 2'
+  },
+  {
+    id: 3,
+    name: 'Recopilación 3'
+  }
+]
