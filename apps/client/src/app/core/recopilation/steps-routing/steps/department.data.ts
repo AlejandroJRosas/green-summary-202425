da4cca0066d31bf22e559f7d56feeb6956670cc2
Chocopaula <mariapforero2003@gmail.com>
2024-06-04 08:39:40 -0400

    refactor: add department data

---
+++ apps/client/src/app/core/recopilation/steps-routing/steps/department.data.ts
@@ -0,0 +1,42 @@
+export const departments = [
+  {
+    name: 'Departamento 1',
+    key: 1
+  },
+  {
+    name: 'Departamento 2',
+    key: 2
+  },
+  {
+    name: 'Departamento 3',
+    key: 3
+  },
+  {
+    name: 'Departamento 4',
+    key: 4
+  },
+  {
+    name: 'Departamento 5',
+    key: 5
+  },
+  {
+    name: 'Departamento 6',
+    key: 6
+  },
+  {
+    name: 'Departamento 7',
+    key: 7
+  },
+  {
+    name: 'Departamento 8',
+    key: 8
+  },
+  {
+    name: 'Departamento 9',
+    key: 9
+  },
+  {
+    name: 'Departamento 10',
+    key: 10
+  }
+]
