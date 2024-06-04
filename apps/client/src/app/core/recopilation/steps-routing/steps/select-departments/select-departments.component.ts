aee6a294f31d63fa47ea270a4036fe4533d88c32
Chocopaula <mariapforero2003@gmail.com>
2024-06-04 08:39:40 -0400

    refactor: add department data

--- apps/client/src/app/core/recopilation/steps-routing/steps/select-departments/select-departments.component.ts
+++ apps/client/src/app/core/recopilation/steps-routing/steps/select-departments/select-departments.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router'
 import { DropdownModule } from 'primeng/dropdown'
 import { CheckboxModule } from 'primeng/checkbox'
 import { recopilations } from '../recopilations.data'
+import { departments } from '../department.data'
 
 @Component({
   selector: 'app-select-departments',
@@ -16,48 +17,7 @@ export class SelectDepartmentsComponent {
   constructor(private router: Router) {}
 
   recopilations = recopilations
-  departments = [
-    {
-      name: 'Departamento 1',
-      key: 1
-    },
-    {
-      name: 'Departamento 2',
-      key: 2
-    },
-    {
-      name: 'Departamento 3',
-      key: 3
-    },
-    {
-      name: 'Departamento 4',
-      key: 4
-    },
-    {
-      name: 'Departamento 5',
-      key: 5
-    },
-    {
-      name: 'Departamento 6',
-      key: 6
-    },
-    {
-      name: 'Departamento 7',
-      key: 7
-    },
-    {
-      name: 'Departamento 8',
-      key: 8
-    },
-    {
-      name: 'Departamento 9',
-      key: 9
-    },
-    {
-      name: 'Departamento 10',
-      key: 10
-    }
-  ]
+  departments = departments
   selectedRecopilation = {}
   selectedDepartments = []
 
