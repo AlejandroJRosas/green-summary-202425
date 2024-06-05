a57db08d3e6d2389ea7d0a47c5438f75fe305bf1
Chocopaula <mariapforero2003@gmail.com>
2024-06-05 18:10:53 -0400

    feat: add data categoriesTotal

---
+++ apps/client/src/app/core/recopilation/steps-routing/steps/recommend-categories-department/categoriesTotal.data.ts
@@ -0,0 +1,16 @@
+import { Category } from '../select-indicators-categories-criteria/categories.data'
+
+export const categoriesTotal: Category[] = [
+  {
+    id: 1,
+    name: 'Categoria 1'
+  },
+  {
+    id: 2,
+    name: 'Categoria 2'
+  },
+  {
+    id: 3,
+    name: 'Categoria 3'
+  }
+]
