58ffe5ff686aa8b2629e951d04cb18fd2fbb84f1
DaikoneKisu <eduardoarzolay@gmail.com>
2024-06-05 16:40:55 -0400

    feat(frontend auth): add validation for expired tokens

--- apps/client/src/app/services/auth.service.ts
+++ apps/client/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { Observable } from 'rxjs'
 import { BaseUrl } from '../../config'
+import { decodeJwtPayload } from '../../utils/decode-jwt-payload'
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,31 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   isAuth(): boolean {
-    return !!localStorage.getItem('token')
+    try {
+      const token = localStorage.getItem('token')
+      //TODO: Refactor this to use a type guard for UserJwtPayload
+      const user: UserJwtPayload = decodeJwtPayload(token) as UserJwtPayload
+      const expDate = new Date(user.exp * 1000)
+
+      if (expDate < new Date()) {
+        return false
+      }
+    } catch (error: unknown) {
+      //TODO: Refactor error handling
+      if (!(error instanceof Error)) {
+        throw error
+      }
+
+      //! - This is a bad practice, we should not rely on the error message
+      if (error.message === 'Invalid jwt token provided') {
+        return false
+      }
+
+      console.error(error)
+      return false
+    }
+
+    return true
   }
 
   isRole(roles: RoleType[]): boolean {
@@ -54,4 +79,12 @@ type User = {
   email: string
   password: string
   type: string
+}
+
+type UserJwtPayload = {
+  id: number
+  name: string
+  type: string
+  iat: number
+  exp: number
 }
