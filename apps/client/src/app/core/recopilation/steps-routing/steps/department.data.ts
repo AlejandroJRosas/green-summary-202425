604d572ebfaeec07c80b1b41f7d9aa02d5445661
Chocopaula <mariapforero2003@gmail.com>
2024-06-05 18:09:31 -0400

    refactor: add type department and change key by id

--- apps/client/src/app/core/recopilation/steps-routing/steps/department.data.ts
+++ apps/client/src/app/core/recopilation/steps-routing/steps/department.data.ts
@@ -1,42 +1,46 @@
-export const departments = [
+export type Departament = {
+  id: number
+  name: string
+}
+export const departments: Departament[] = [
   {
     name: 'Departamento 1',
-    key: 1
+    id: 1
   },
   {
     name: 'Departamento 2',
-    key: 2
+    id: 2
   },
   {
     name: 'Departamento 3',
-    key: 3
+    id: 3
   },
   {
     name: 'Departamento 4',
-    key: 4
+    id: 4
   },
   {
     name: 'Departamento 5',
-    key: 5
+    id: 5
   },
   {
     name: 'Departamento 6',
-    key: 6
+    id: 6
   },
   {
     name: 'Departamento 7',
-    key: 7
+    id: 7
   },
   {
     name: 'Departamento 8',
-    key: 8
+    id: 8
   },
   {
     name: 'Departamento 9',
-    key: 9
+    id: 9
   },
   {
     name: 'Departamento 10',
-    key: 10
+    id: 10
   }
 ]
