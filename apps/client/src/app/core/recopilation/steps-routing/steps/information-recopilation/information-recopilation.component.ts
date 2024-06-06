fdc56e1d5d83766f9c8d753c3b3c77fd2b8d51fb
Chocopaula <mariapforero2003@gmail.com>
2024-06-06 18:11:01 -0400

    refactor: change structure in html and add tooltip in dates inputs

--- apps/client/src/app/core/recopilation/steps-routing/steps/information-recopilation/information-recopilation.component.ts
+++ apps/client/src/app/core/recopilation/steps-routing/steps/information-recopilation/information-recopilation.component.ts
@@ -4,16 +4,26 @@ import { ButtonModule } from 'primeng/button'
 import { CalendarModule } from 'primeng/calendar'
 import { InputTextModule } from 'primeng/inputtext'
 import { InputTextareaModule } from 'primeng/inputtextarea'
+import { TooltipModule } from 'primeng/tooltip'
 
 @Component({
   selector: 'app-information-recopilation',
   standalone: true,
-  imports: [ButtonModule, InputTextModule, InputTextareaModule, CalendarModule],
+  imports: [
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    CalendarModule,
+    TooltipModule
+  ],
   templateUrl: './information-recopilation.component.html'
 })
 export class InformationRecopilationComponent {
   constructor(private router: Router) {}
   nextStep() {
     this.router.navigateByUrl('recopilations/steps-create/select-departments')
+  }
+  prevStep() {
+    this.router.navigateByUrl('recopilations')
   }
 }
