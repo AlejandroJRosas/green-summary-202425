04f812bedc3ca2751d0dbdce53a817f387278fb8
Chocopaula <mariapforero2003@gmail.com>
2024-06-06 18:14:54 -0400

    feat: add button finish later in header, and add step preview

--- apps/client/src/app/core/recopilation/steps-routing/steps-routing.component.ts
+++ apps/client/src/app/core/recopilation/steps-routing/steps-routing.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Inject, OnInit } from '@angular/core'
 import { RouterLink, RouterOutlet } from '@angular/router'
 import { MenuItem } from 'primeng/api'
+import { ButtonModule } from 'primeng/button'
 import { StepsModule } from 'primeng/steps'
+import { Router } from '@angular/router'
+import { Toast } from '../../../common/toast/toast.component'
 
 @Component({
   selector: 'app-steps-routing',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, StepsModule],
+  imports: [RouterOutlet, RouterLink, StepsModule, ButtonModule],
   templateUrl: './steps-routing.component.html',
   styles: ``
 })
 export class StepsRoutingComponent implements OnInit {
+  constructor(
+    private router: Router,
+    @Inject(Toast) private toast: Toast
+  ) {}
   items: MenuItem[] = []
   urlSteps = '/recopilations/steps-create/'
 
   ngOnInit() {
     this.items = [
       {
-        label: 'Crear Recopilación',
+        label: 'Crear recopilación',
         routerLink: `${this.urlSteps}information-recopilation`
       },
       {
@@ -25,13 +32,25 @@ export class StepsRoutingComponent implements OnInit {
         routerLink: `${this.urlSteps}select-departments`
       },
       {
-        label: 'Seleccionar indicadores, categorías y criterios ',
+        label: 'Seleccionar esquema y asociar',
         routerLink: `${this.urlSteps}select-indicators-categories-criteria`
       },
       {
         label: 'Recomendar categorías a departamentos',
         routerLink: `${this.urlSteps}recommend-categories-department`
+      },
+      {
+        label: 'Previsualizar',
+        routerLink: `${this.urlSteps}preview`
       }
     ]
+  }
+  onFinishLater() {
+    this.router.navigateByUrl('recopilations')
+    this.toast.show(
+      'info',
+      'Terminar Después',
+      'Puedes terminar la recopilación en otro momento'
+    )
   }
 }
