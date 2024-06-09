c489bb6f8e58c5dd55ff9664c10830fbbfbc8310
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-06-09 14:29:45 -0400

    fix: make unique indicator per recopilation

--- apps/server/src/core/indicators-per-recopilations/entities/indicator-per-recopilatio.entity.ts
+++ apps/server/src/core/indicators-per-recopilations/entities/indicator-per-recopilatio.entity.ts
@@ -1,8 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, ManyToOne, Unique } from 'typeorm'
 import { Indicator } from 'src/core/indicators/entities/indicator.entity'
 import { Recopilation } from 'src/core/recopilations/entities/recopilation.entity'
 
 @Entity('indicators_per_recopilations')
+@Unique(['recopilation', 'indicator'])
 export class IndicatorPerRecopilation {
   @PrimaryGeneratedColumn()
   id: number
