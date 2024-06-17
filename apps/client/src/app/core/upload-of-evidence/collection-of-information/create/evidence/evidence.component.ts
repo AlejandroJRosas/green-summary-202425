999dd5e6e11adadfff4c18b4881783fd80a5b520
Laboratorio BD - Equipo 15 <labbd-15@guayanalabs.ucab.edu.ve>
2024-06-17 11:57:21 -0400

    feat: add form group

--- apps/client/src/app/core/upload-of-evidence/collection-of-information/create/evidence/evidence.component.ts
+++ apps/client/src/app/core/upload-of-evidence/collection-of-information/create/evidence/evidence.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core'
 import { ButtonModule } from 'primeng/button'
-import { DropdownModule } from 'primeng/dropdown'
+import { DropdownChangeEvent, DropdownModule } from 'primeng/dropdown'
 import { EvidenceDocumentComponent } from './types/evidence-document/evidence-document.component'
 import { EvidenceImageComponent } from './types/evidence-image/evidence-image.component'
 import { EvidenceLinkComponent } from './types/evidence-link/evidence-link.component'
 import { FieldsetModule } from 'primeng/fieldset'
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms'
 
 @Component({
   selector: 'evidence',
@@ -15,20 +21,36 @@ import { FieldsetModule } from 'primeng/fieldset'
     EvidenceDocumentComponent,
     EvidenceImageComponent,
     EvidenceLinkComponent,
-    FieldsetModule
+    FieldsetModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './evidence.component.html',
   styles: ``
 })
 export class EvidenceComponent {
+  formGroup = new FormGroup({
+    selectedType: new FormControl<typeEvidence>({
+      name: '',
+      code: ''
+    })
+  })
+
   typesOfEvidence = [
     { name: 'Imagen', code: 'image' },
     { name: 'Documento', code: 'document' },
     { name: 'Link', code: 'link' }
   ]
-  selectedType = 'document'
   evidences: number[] = []
   addEvidence() {
     this.evidences.push(this.evidences.length + 1)
   }
+  onChange(event: DropdownChangeEvent) {
+    console.log(event)
+  }
+}
+
+interface typeEvidence {
+  name: string
+  code: string
 }
