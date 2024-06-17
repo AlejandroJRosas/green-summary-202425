dabb0d919394b20d64f4b3f4b2309a01a970b276
Chocopaula <mariapforero2003@gmail.com>
2024-06-17 00:20:47 -0400

    feat: add upload of evidence Component
    this component handles the first page that the department sees, it checks that the category obtained from the url is correct, if it is not correct it indicates 404 not found, it calls the information collection component passing the name of the indicator and the category.

---
+++ apps/client/src/app/core/upload-of-evidence/data.ts
@@ -0,0 +1,16 @@
+export const collectionInformation = [
+  {
+    id: 1,
+    recopilationId: 1,
+    categoryId: 7,
+    name: 'Collection of information 1',
+    summary: 'Summary Collection Of Information'
+  },
+  {
+    id: 2,
+    recopilationId: 1,
+    categoryId: 7,
+    name: 'Collection of information 2',
+    summary: 'Summary Collection Of Information 2'
+  }
+]
---
+++ apps/client/src/app/core/upload-of-evidence/upload-of-evidence.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { ButtonModule } from 'primeng/button'
+import { CategoryService } from '../../services/category.service'
+import { Router } from '@angular/router'
+import { CollectionOfInformationComponent } from './collection-of-information/collection-of-information.component'
+import { ProgressSpinnerModule } from 'primeng/progressspinner'
+
+@Component({
+  selector: 'app-upload-of-evidence',
+  standalone: true,
+  imports: [
+    ButtonModule,
+    CollectionOfInformationComponent,
+    ProgressSpinnerModule
+  ],
+  templateUrl: './upload-of-evidence.html',
+  styles: ``
+})
+export class UploadOfEvidenceComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private router: Router
+  ) {
+    this.route.params.subscribe((params) => {
+      this.categoryId = parseInt(params['categoryId'], 10)
+      this.recopilationId = parseInt(params['recopilationId'], 10)
+    })
+  }
+  recopilationId: number = 0
+  categoryId: number = 0
+  indicatorName: string = ''
+  categoryName: string = ''
+  fetchCategoryById = false
+  ngOnInit() {
+    this.getCategoryById()
+  }
+  getCategoryById() {
+    this.fetchCategoryById = true
+    this.categoryService.getById(this.categoryId).subscribe({
+      next: (res) => {
+        if (res.status === 'success') {
+          this.indicatorName = res.data.indicator.alias
+          this.categoryName = res.data.name
+          this.fetchCategoryById = false
+        }
+      },
+      error: (e) => {
+        console.error(e)
+        this.fetchCategoryById = false
+        this.router.navigateByUrl('/404 Not Found')
+      }
+    })
+  }
+}
