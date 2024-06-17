ba209e231a1af5cd1d6507b19c91c3758628bd15
Chocopaula <mariapforero2003@gmail.com>
2024-06-17 00:26:06 -0400

    feat: add create collection component
    This component manages the information of a collection, which is its description and has the buttons to cancel the creation or save, it also contains the Evidence component.

---
+++ apps/client/src/app/core/upload-of-evidence/collection-of-information/create/create-collection.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+import { InputTextareaModule } from 'primeng/inputtextarea'
+import { ButtonModule } from 'primeng/button'
+import { EvidenceComponent } from './evidence/evidence.component'
+
+@Component({
+  selector: 'create-collection',
+  standalone: true,
+  imports: [InputTextareaModule, ButtonModule, EvidenceComponent],
+  templateUrl: './create-collection.component.html',
+  styles: ``
+})
+export class CreateCollectionComponent {}
