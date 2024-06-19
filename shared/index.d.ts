52ec2f6b4860539de0a463e8e50e29fb6c2bebb1
AlejandroJRosas <alejandrojrosas2001@gmail.com>
2024-06-19 17:35:25 -0400

    chore: delete not used index files

--- shared/index.d.ts
+++
@@ -1,9 +0,0 @@
-export as namespace shared
-
-export interface Config {
-  nombreAplicacion: string
-  apiUrl: string
-  apiKey: string
-}
-
-export const config: Config
--- shared/index.js
+++
@@ -1,5 +0,0 @@
-export const config = {
-  nombreAplicacion: 'Mi aplicación',
-  apiUrl: 'https://api.miaplicacion.com',
-  apiKey: 'my-api-key'
-}
