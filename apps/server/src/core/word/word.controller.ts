6a2df2797f8cf67cdc5ef4fb9a3996cb0495af08
Edusus <ejsucre.19@est.ucab.edu.ve>
2024-06-29 19:43:46 -0400

    endpoint to obtain the information

--- apps/server/src/core/word/word.controller.ts
+++ apps/server/src/core/word/word.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Param, Res } from '@nestjs/common'
 import { WordService } from './word.service'
 import { ApiTags } from '@nestjs/swagger'
 import { Roles } from '../auth/roles.decorator'
@@ -10,8 +10,29 @@ import { Role } from '../auth/role.enum'
 export class WordController {
   constructor(private readonly wordService: WordService) {}
 
-  @Get()
-  findAll() {
-    return this.wordService.findAll()
+  @Get(':idCrit/:idRecop')
+  async generateWord(
+    @Param('idCrit') idCrit: number,
+    @Param('idRecop') idRecop: number,
+    @Res() res
+  ) {
+    try {
+      const prueba = await this.wordService.findInformationCollections(
+        idCrit,
+        idRecop
+      )
+      //const buffer = await this.wordService.findCategory(idCrit,idRecop);
+
+      // res.set({
+      //   'Content-Type':'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      //   'Content-Disposition': 'attachment; filename=archivo.docx',
+      //   'Content-Length': buffer.length
+      // })
+      //res.send(buffer)
+      res.send(prueba)
+    } catch (e) {
+      console.log(e)
+      throw e
+    }
   }
 }
--- apps/server/src/core/word/word.service.ts
+++ apps/server/src/core/word/word.service.ts
@@ -1,38 +1,82 @@
 import { Injectable } from '@nestjs/common'
-import { Document, Packer, Paragraph, TextRun } from 'docx'
+import { Document, Packer, Paragraph, SectionType, TextRun } from 'docx'
 // import { saveAs } from 'file-saver';
-import * as fs from 'fs'
+// import * as fs from 'fs'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { InformationCollection } from '../information-collections/entities/information-collection.entity'
+import { CategorizedCriteria } from '../categorized-criteria/entities/categorized-criterion.entity'
+import { Criteria } from '../criterion/entities/criteria.entity'
+import { Recopilation } from '../recopilations/entities/recopilation.entity'
 
 @Injectable()
 export class WordService {
-  findAll() {
+  constructor(
+    @InjectRepository(InformationCollection)
+    private informationCollectionsRepository: Repository<InformationCollection>,
+    @InjectRepository(Criteria)
+    private criteriaRepository: Repository<Criteria>,
+    @InjectRepository(Recopilation)
+    private recopilationRepository: Repository<Recopilation>,
+    @InjectRepository(CategorizedCriteria)
+    private categorizedCriteriaRepository: Repository<CategorizedCriteria>
+  ) {}
+
+  async findInformationCollections(idCrit: number, idRecop: number) {
+    const [recopilation, criterion] = await Promise.all([
+      this.recopilationRepository.findOneByOrFail({
+        id: idRecop
+      }),
+      this.criteriaRepository.findOneByOrFail({
+        id: idCrit
+      })
+    ])
+
+    const categorizedCriteria =
+      await this.categorizedCriteriaRepository.findOneOrFail({
+        where: {
+          recopilation: { id: recopilation.id },
+          criteria: { id: criterion.id }
+        },
+        relations: ['recopilation', 'category', 'criteria']
+      })
+
+    const informationCollections =
+      await this.informationCollectionsRepository.find({
+        where: {
+          recopilation: (await categorizedCriteria).recopilation,
+          category: (await categorizedCriteria).category
+        },
+        relations: ['department', 'evidences']
+      })
+
+    return { criterion, recopilation, informationCollections }
+  }
+
+  async generateWord(idCrit: number, idRecop: number) {
     const doc = new Document({
       sections: [
         {
-          properties: {},
+          properties: {
+            type: SectionType.CONTINUOUS
+          },
           children: [
             new Paragraph({
-              children: [
-                new TextRun('Hello World'),
-                new TextRun({
-                  text: 'Foo Bar',
-                  bold: true
-                }),
-                new TextRun({
-                  text: '\tGithub is the best',
-                  bold: true
-                })
-              ]
+              children: [new TextRun('HOLA MUNDO ' + idCrit)]
+            }),
+            new Paragraph({
+              children: [new TextRun('ADIOS MUNDO CRUEL! ' + idRecop)]
+            }),
+            new Paragraph({
+              children: [new TextRun('')]
             })
           ]
         }
       ]
     })
 
-    Packer.toBuffer(doc).then((buffer) => {
-      fs.writeFileSync('My Document.docx', buffer)
-    })
+    const buffer = await Packer.toBuffer(doc)
 
-    return
+    return buffer
   }
 }
