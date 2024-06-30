a49e74161fb26409e189a60214fdd856d3a07f79
Edusus <ejsucre.19@est.ucab.edu.ve>
2024-06-29 20:11:39 -0400

    fix: settings in the summary-information.module

--- apps/server/src/core/summary-information/summary-information.module.ts
+++ apps/server/src/core/summary-information/summary-information.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common'
 import { SummaryInformationService } from './summary-information.service'
 import { SummaryInformationController } from './summary-information.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { InformationCollection } from '../information-collections/entities/information-collection.entity'
+import { CategorizedCriteria } from '../categorized-criteria/entities/categorized-criterion.entity'
+import { Criteria } from '../criterion/entities/criteria.entity'
+import { Recopilation } from '../recopilations/entities/recopilation.entity'
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      InformationCollection,
+      CategorizedCriteria,
+      Criteria,
+      Recopilation
+    ])
+  ],
   controllers: [SummaryInformationController],
   providers: [SummaryInformationService]
 })
