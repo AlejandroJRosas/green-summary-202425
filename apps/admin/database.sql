d5c810d82bacb02d827cacf06b6dde066864219d
AlejandroJRosas <alejandrojrosas2001@gmail.com>
2024-07-06 18:20:19 -0400

    changes in model definition

--- apps/admin/database.sql
+++ apps/admin/database.sql
@@ -1,80 +1,119 @@
+BEGIN;
+
 CREATE TABLE time (
-  date TIMESTAMP PRIMARY KEY
+  tiempo timestamp PRIMARY KEY
 );
 
 CREATE TABLE recopilations (
-  'recopilationId' INT PRIMARY KEY,
-  name VARCHAR(30),
+  "recopilationId" INT PRIMARY KEY,
+  name TEXT,
   description TEXT,
-  'startDate' DATE,
-  'endDate' DATE,
-  'departmentEndDate' DATE
+  "startDate" DATE,
+  "endDate" DATE,
+  "departmentEndDate" DATE,
+  "isReady" BOOLEAN
 );
 
 CREATE TABLE departments (
-  'departmentId' INT PRIMARY KEY,
-  name VARCHAR(255),
-  email VARCHAR(255),
-  type VARCHAR(255)
+  "departmentId" INT PRIMARY KEY,
+  name TEXT,
+  email TEXT
 );
 
 CREATE TABLE indicators (
-  'index' INT PRIMARY KEY,
-  name VARCHAR(255),
-  alias VARCHAR(255),
-  'helpText' TEXT
+  "index" INT PRIMARY KEY,
+  name TEXT,
+  alias TEXT,
+  "helpText" TEXT
 );
 
 CREATE TABLE categories (
-  'categoryId' INT PRIMARY KEY,
-  name VARCHAR(50),
-  'helpText' TEXT
+  "indicatorIndex" INT,
+  "categoryId" INT,
+  name TEXT,
+  "helpText" TEXT,
+  PRIMARY KEY ("indicatorIndex", "categoryId")
 );
 
 CREATE TABLE criteria (
-  'criteriaId' INT,
-  'indicatorIndex' INT,
-  'subIndex' INT,
-  name VARCHAR(255),
-  alias VARCHAR(255),
-  'helpText' TEXT,
-  'requiresEvidence' BOOLEAN,
-  PRIMARY KEY ('indicatorIndex', 'subIndex'),
-  CONSTRAINT fk_indicator FOREIGN KEY ('indicatorIndex') REFERENCES indicators('index')
+  "criteriaId" INT,
+  "indicatorIndex" INT,
+  "subIndex" INT,
+  name TEXT,
+  alias TEXT,
+  "helpText" TEXT,
+  "requiresEvidence" BOOLEAN,
+  PRIMARY KEY ("indicatorIndex", "subIndex")
 );
 
-CREATE TABLE InformationCollection (
-  ID INT PRIMARY KEY,
-  Summary TEXT,
-  'createAt' DATE
+CREATE TABLE "informationCollection" (
+  "collectionId" INT PRIMARY KEY,
+  name TEXT,
+  summary TEXT,
+  "createAt" DATE,
+  "recopilationId" INT,
+  "departmentId" INT,
+  "categoryId" INT
 );
 
-CREATE TABLE Evidences (
-  'evidenceId' INT PRIMARY KEY,
+CREATE TABLE evidences (
+  "evidenceId" INT PRIMARY KEY,
   description TEXT,
-  type ,
-  'externalLink' TEXT,
-  'fileLink' TEXT,
-  'uploadDate' DATE,
+  type VARCHAR(32),
+  "externalLink" TEXT,
+  "fileLink" TEXT,
+  "createdAt" DATE,
   error TEXT
 );
 
-CREATE TABLE FACT_Answers (
-  'answerId' INT PRIMARY KEY,
-  'recopilationId' INT,
-  'departmentId' INT,
-  'categoryId' INT,
-  'index' INT,
-  'subIndex' INT,
-  'collectionID' INT,
-  'evidenceId' INT,
+CREATE TABLE answers (
+  "answerId" INT PRIMARY KEY,
+  "recopilationId" INT,
+  "departmentId" INT,
+  "categoryId" INT,
+  "collectionId" INT,
+  "index" INT,
+  "indicatorIndex" INT,
+  "subIndex" INT,
+  "evidenceId" INT,
+  tiempo timestamp,
   wasRecommended BOOLEAN,
-  FOREIGN KEY (TimeID) REFERENCES Time(ID),
-  FOREIGN KEY ('recopilationId') REFERENCES recopilations('recopilationId'),
-  FOREIGN KEY ('departmentId') REFERENCES departments ('departmentId'),
-  FOREIGN KEY ('index') REFERENCES indicators ('index'),
-  FOREIGN KEY ('criteriaId') REFERENCES criteria('criteriaId'),
-  FOREIGN KEY ('categoryId') REFERENCES categories('categoryId'),
-  FOREIGN KEY ('collectionID') REFERENCES InformationCollection(ID),
-  FOREIGN KEY ('evidenceId') REFERENCES Evidences('evidenceId')
+  FOREIGN KEY (tiempo) REFERENCES time (tiempo),
+  FOREIGN KEY ("recopilationId") REFERENCES recopilations("recopilationId"),
+  FOREIGN KEY ("departmentId") REFERENCES departments ("departmentId"),
+  FOREIGN KEY ("index") REFERENCES indicators ("index"),
+  FOREIGN KEY ("indicatorIndex", "subIndex") REFERENCES criteria("indicatorIndex", "subIndex"),
+  FOREIGN KEY ("indicatorIndex", "categoryId") REFERENCES categories("indicatorIndex", "categoryId"),
+  FOREIGN KEY ("collectionId") REFERENCES "informationCollection"("collectionId"),
+  FOREIGN KEY ("evidenceId") REFERENCES evidences ("evidenceId")
 );
+
+COMMIT;
+
+-- SELECT 
+--   ans.id AS "answerId",
+--   ans."recopilationId",
+--   ans."categoryId",
+--   ans."departmentId",
+--   ans."informationCollectionId",
+--   e.id AS "evidenceId",
+--   c."indicatorIndex",
+--   c."subIndex",
+--   CASE
+--     WHEN rec.id IS NOT NULL THEN TRUE
+--     ELSE FALSE
+--   END AS "wasRecommended"
+-- FROM answers AS ans
+-- INNER JOIN categorized_criterion AS cc ON
+--   ans."recopilationId" = cc."recopilationId" AND
+--   ans."categoryId" = cc."categoryId"
+-- INNER JOIN evidences AS e ON
+--   ans."informationCollectionId" = e."collectionId"
+-- INNER JOIN criterion AS c ON
+--   cc."criteriaId" = c.id
+-- INNER JOIN departments_per_recopilations AS dpr ON
+--   ans."recopilationId" = dpr."recopilationId" AND
+--   ans."departmentId" = dpr."departmentId"
+-- LEFT JOIN recommendations AS rec ON
+--   dpr.id = rec."departmentPerRecopilationId" AND
+--   rec."categoryId" = ans."categoryId"
