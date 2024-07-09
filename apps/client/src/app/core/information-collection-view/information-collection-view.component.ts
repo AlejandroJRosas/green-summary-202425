74b30596a6721ae1ef8f921f4a4b93f91646ef59
Chocopaula <mariapforero2003@gmail.com>
2024-07-09 00:03:42 -0400

    feat: add error popup

--- apps/client/src/app/core/information-collection-view/information-collection-view.component.ts
+++ apps/client/src/app/core/information-collection-view/information-collection-view.component.ts
@@ -17,6 +17,9 @@ import { Indicator } from '../../../shared/types/indicator.type'
 import { CategoryService } from '../../services/category.service'
 import { DepartmentService } from '../../services/department.service'
 import { User } from '../../../shared/types/user.type'
+import { ConfirmPopupModule } from 'primeng/confirmpopup'
+import { ConfirmationService, MessageService } from 'primeng/api'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-information-collection-view',
@@ -28,8 +31,11 @@ import { User } from '../../../shared/types/user.type'
     InputTextareaModule,
     InputTextModule,
     ImageModule,
-    DividerModule
+    DividerModule,
+    ConfirmPopupModule,
+    ReactiveFormsModule
   ],
+  providers: [ConfirmationService, MessageService],
   templateUrl: './information-collection-view.component.html',
   styles: ``
 })
@@ -40,7 +46,9 @@ export class InformationCollectionViewComponent implements OnInit {
     private InformationCollectionService: InformationCollectionService,
     private CategoryService: CategoryService,
     private router: Router,
-    private DepartmentService: DepartmentService
+    private DepartmentService: DepartmentService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
   ) {
     this.route.params.subscribe((params) => {
       this.categoryId = parseInt(params['categoryId'], 10)
@@ -51,6 +59,9 @@ export class InformationCollectionViewComponent implements OnInit {
   recopilationId: number = 0
   categoryId: number = 0
   departmentId: number = 0
+  formGroup = new FormGroup({
+    error: new FormControl('')
+  })
   informationCollections: InformationCollectionByDepartment[] = []
   indicator: Indicator = {
     index: 0,
@@ -85,6 +96,30 @@ export class InformationCollectionViewComponent implements OnInit {
       default:
         return 'Link'
     }
+  }
+  confirm(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Please confirm to proceed moving forward.',
+      icon: 'pi pi-exclamation-circle',
+      acceptIcon: 'pi pi-check mr-1',
+      rejectIcon: 'pi pi-times mr-1',
+      acceptLabel: 'Guardar',
+      rejectLabel: 'Cancelar',
+      rejectButtonStyleClass: 'p-button-outlined p-button-sm',
+      acceptButtonStyleClass: 'p-button-sm',
+      accept: () => {
+        this.toast.show('success', 'Guardando...', 'Guardando el error...')
+      },
+      reject: () => {
+        this.toast.show(
+          'error',
+          'Rechazado',
+          'Ha rechazado la subida del error'
+        )
+        this.formGroup.controls.error.setValue('')
+      }
+    })
   }
   getDepartmentById() {
     this.DepartmentService.getById(this.departmentId).subscribe({
