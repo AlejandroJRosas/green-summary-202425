e79935dffc249ea57dade940dafb2ecd9d425c4f
Chocopaula <mariapforero2003@gmail.com>
2024-07-14 20:47:44 -0400

    feat: add overlay notifications

--- apps/client/src/app/common/body/body.component.ts
+++ apps/client/src/app/common/body/body.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
+import { ButtonModule } from 'primeng/button'
+import { OverlayPanelModule } from 'primeng/overlaypanel'
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, OverlayPanelModule],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
