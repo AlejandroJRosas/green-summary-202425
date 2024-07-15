b7bf56257713c74877df64f6544f15903fee09ed
Chocopaula <mariapforero2003@gmail.com>
2024-07-15 00:08:54 -0400

    feat: perdió colombia :(

--- apps/client/src/app/common/body/body.component.ts
+++ apps/client/src/app/common/body/body.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
 import { ButtonModule } from 'primeng/button'
 import { OverlayPanelModule } from 'primeng/overlaypanel'
+import { NotificationComponent } from '../../core/notification/notification.component'
+import { BadgeModule } from 'primeng/badge'
+import { AvatarModule } from 'primeng/avatar'
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule, OverlayPanelModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ButtonModule,
+    OverlayPanelModule,
+    NotificationComponent,
+    BadgeModule,
+    AvatarModule
+  ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
-export class BodyComponent {}
+export class BodyComponent implements OnInit {
+  user = {
+    fullName: '',
+    email: ''
+  }
+  ngOnInit() {
+    this.getDataLocalStorage()
+  }
+  getDataLocalStorage() {
+    this.user.fullName = JSON.parse(localStorage.getItem('user')!).fullName
+    this.user.email = JSON.parse(localStorage.getItem('user')!).email
+  }
+}
