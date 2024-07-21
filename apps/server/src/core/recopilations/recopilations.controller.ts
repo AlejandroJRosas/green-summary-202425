232dde5610d957f607e2a53a986982c214338375
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2024-07-21 04:07:49 -0400

    fix: del mejor feature del mundo

--- apps/server/src/core/recopilations/recopilations.controller.ts
+++ apps/server/src/core/recopilations/recopilations.controller.ts
@@ -228,12 +228,12 @@ export class RecopilationsController {
     const res = await this.recopilationsService.recommendCategories(
       recommendCategoriesDto
     )
-
+    console.log('1')
     await this.eventEmitter.emitAsync(
       'matrix.changed',
       new MatrixChangedEvent(recommendCategoriesDto.recopilationId)
     )
-
+    console.log('2')
     return res
   }
 
@@ -346,7 +346,7 @@ export class RecopilationsController {
     return this.recopilationsService.getRecopilationStatistics(+id)
   }
 
-  @OnEvent('matrix.changed', { async: true })
+  @OnEvent('matrix.changed', { async: true, promisify: true })
   handleChangedMatrixEvent(payload: MatrixChangedEvent) {
     return this.recopilationsService.constructAndSaveMatrix(
       payload.recopilationId
